1bbfa88f889199aef29f7a7dde39ebb17a4efcb3
imran-saleh110 <imranforwork110@gmail.com>
2026-06-22 01:06:16 +0600

    optimized version of tic,tac,toe

--- tic,tac,toe.c
+++ tic,tac,toe.c
@@ -1,143 +1,126 @@
 #include <stdio.h>
 #include <stdbool.h>
 
-void print(char a[]);
+#define BOARD_SIZE 9
+#define EMPTY ' '
+#define PLAYER_ONE 1
+#define PLAYER_TWO 2
+#define DRAW 0
+
+void print_board(char a[]);
 bool equality(char a, char b, char c);
+int win(char a[]);
 
 int main() {
-    char a[9] = {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '};
+    char board[BOARD_SIZE] = {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '};
     printf("Welcome to terminal tick, tac, toe!\n\n");
-    print(a);
-    
-    int p1_move = 0;
-    int p2_move = 0;
-    
-    int turn = 1;
-    while(true) {
-        switch(turn) {
-            case 1:
-            {
-                printf("Player 1: Enter placement in the grid(1-9): ");
-                scanf("%d", &p1_move);
-                bool controller = true;
-                while(controller) {
-                    if(!(p1_move > 0 && p1_move < 10)) {
-                        printf("Invallid grid position.\nPlayer 1: Try Again! : ");
-                        scanf("%d", &p1_move);
-                    }
-                    else if(a[p1_move-1] == 'X' || a[p1_move-1] == 'O') {
-                        printf("Position Already Taken.\nPlayer 1: Try Again! : ");
-                        scanf("%d", &p1_move);
-                    }
-                    else {
-                        controller = false;
-                    }
-                }
+    print_board(board);
 
-                a[p1_move-1] = 'X';
-                
-                printf("\n");
-                print(a);
-                turn = 2;
-                break;
-            }
-            case 2:
-            {
-                printf("Player 2: Enter placement in the grid(1-9): ");
-                scanf("%d", &p2_move);
-                bool controller = true;
-                while(controller) {
-                    if(!(p2_move > 0 && p2_move < 10)) {
-                        printf("Invallid grid position.\nPlayer 2: Try Again! : ");
-                        scanf("%d", &p2_move);
-                    }
-                    else if(a[p2_move-1] == 'X' || a[p2_move-1] == 'O') {
-                        printf("Position Already Taken.\nPlayer 2: Try Again! : ");
-                        scanf("%d", &p2_move);
-                    }
-                    else {
-                        controller = false;
-                    }
-                }
-                
-                a[p2_move-1] = 'O';
-                
-                printf("\n");
-                print(a);
-                turn = 1;
-                break;
-            }
+    int players_move;
+    int which_player = PLAYER_ONE;
+
+    while(true) {
+      printf("Player %d: Enter placement in the grid(1-9): ", which_player);
+      scanf("%d", &players_move);
+
+      bool wait_for_valid_input = true;
+      while(wait_for_valid_input) {
+        if(!(players_move > 0 && players_move < 10)) {
+          printf("Invallid grid position.\nPlayer %d: Try Again! : ", which_player);
+          scanf("%d", &players_move);
         }
-        
-        // Winning condition
-        // Might find a better solution later
-        
-        // Checking rows
-        for(int i = 0; i < 7; i = i + 3) {
-            if(equality(a[i], a[i+1], a[i+2])) {
-                if(a[i] == 'X') {
-                    printf("Player 1 Won!\n");
-                    return 0;
-                }
-                else if(a[i] == 'O') {
-                    printf("Player 2 Won!\n");
-                    return 0;
-                }
-            }
+        else if(board[players_move-1] == 'X' || board[players_move-1] == 'O') {
+          printf("Position Already Taken.\nPlayer %d: Try Again! : ", which_player);
+          scanf("%d", &players_move);
         }
-        
-        // Checking columns
-        for(int i = 0; i < 3; i++) {
-            if(equality(a[i], a[i+3], a[i+6])) {
-                if(a[i] == 'X') {
-                    printf("Player 1 Won!\n");
-                    return 0;
-                }
-                else if(a[i] == 'O') {
-                    printf("Player 2 Won!\n");
-                    return 0;
-                }
-            }
+        else {
+          wait_for_valid_input = false;
         }
-        
-        // Checking diagonals
-            if(equality(a[0], a[4], a[8])) {
-                if(a[0] == 'X') {
-                    printf("Player 1 Won!\n");
-                    return 0;
-                }
-                else if(a[0] == 'O') {
-                    printf("Player 2 Won!\n");
-                    return 0;
-                }
-            }
-            
-            if(equality(a[2], a[4], a[6])) {
-                if(a[2] == 'X') {
-                    printf("Player 1 Won!\n");
-                    return 0;
-                }
-                else if(a[2] == 'O') {
-                    printf("Player 2 Won!\n");
-                    return 0;
-                }
-            }
-            
-            // Checking for draw
-            bool flag = false;
-            for(int i = 0; i < 9; i++) {
-                if(a[i] == ' ')
-                    flag = true;
-            }
-            
-            if(!flag) {
-                printf("Draw!");
-                return 0;
-            }
+      }
+
+      board[players_move-1] = which_player == PLAYER_ONE ? 'X' : 'O';
+      printf("\n");
+      print_board(board);
+      which_player = which_player == PLAYER_ONE ? PLAYER_TWO : PLAYER_ONE;
+
+      int w = win(board);
+
+      switch(w) {
+        case PLAYER_ONE:
+          printf("Player 1 Won!\n");
+          return 0;
+        case PLAYER_TWO:
+          printf("Player 2 Won!\n");
+          return 0;
+        case DRAW:
+          printf("Draw!");
+          return 0;
+      }
     }
 }
 
-void print(char a[]) {
+// Winning condition
+int win(char a[]) {
+// Checking rows
+  for(int i = 0; i < 7; i = i + 3) {
+    if(equality(a[i], a[i+1], a[i+2])) {
+      if(a[i] == 'X') {
+        return PLAYER_ONE;
+      }
+      else if(a[i] == 'O') {
+        return PLAYER_TWO;
+      }
+    }
+  }
+
+  // Checking columns
+  for(int i = 0; i < 3; i++) {
+    if(equality(a[i], a[i+3], a[i+6])) {
+      if(a[i] == 'X') {
+        return PLAYER_ONE;
+      }
+      else if(a[i] == 'O') {
+        return PLAYER_TWO;
+      }
+    }
+  }
+
+  // Checking diagonals
+  if(equality(a[0], a[4], a[8])) {
+    if(a[0] == 'X') {
+      return PLAYER_ONE;
+    }
+    else if(a[0] == 'O') {
+      return PLAYER_TWO;
+    }
+  }
+
+  if(equality(a[2], a[4], a[6])) {
+    if(a[2] == 'X') {
+      return PLAYER_ONE;
+    }
+    else if(a[2] == 'O') {
+      return PLAYER_TWO;
+    }
+  }
+
+  // Checking for draw
+  bool flag = false;
+  for(int i = 0; i < BOARD_SIZE; i++) {
+    if(a[i] == ' ') {
+      flag = true;
+      break;
+    }
+  }
+  if(!flag) {
+    return 0;
+  }
+
+  // To continue the loop if its none of the above
+  return 3;
+}
+
+void print_board(char a[]) {
     printf("#------Grid------#\n\n");
     printf("  %c  |  %c  |  %c  \n", a[0], a[1], a[2]);
     printf("-----+-----+-----\n");
@@ -147,73 +130,6 @@ void print(char a[]) {
 }
 
 bool equality(char a, char b, char c) {
-    if(a != ' ' && a == b && b == c) return true;
+    if(a != EMPTY && a == b && b == c) return true;
     else return false;
-}
-
-
-
-// New improved tic tac toe
-// With less LoC
-
-#include <stdio.h>
-#include <stdbool.h>
-
-void print(char b[]);
-bool win(char b[]);
-
-int main() {
-    char b[9] = {' '};
-    int move, turn = 0;
-
-    printf("Welcome to terminal tic-tac-toe!\n\n");
-
-    while (1) {
-        print(b);
-
-        char mark = turn % 2 ? 'O' : 'X';
-        int player = turn % 2 ? 2 : 1;
-
-        do {
-            printf("Player %d (%c): ", player, mark);
-            scanf("%d", &move);
-        } while (move < 1 || move > 9 || b[move - 1] != ' ');
-
-        b[move - 1] = mark;
-
-        if (win(b)) {
-            print(b);
-            printf("Player %d Won!\n", player);
-            break;
-        }
-
-        if (++turn == 9) {
-            print(b);
-            printf("Draw!\n");
-            break;
-        }
-    }
-}
-
-void print(char b[]) {
-    printf("\n %c | %c | %c\n---+---+---\n %c | %c | %c\n---+---+---\n %c | %c | %c\n\n",
-           b[0], b[1], b[2],
-           b[3], b[4], b[5],
-           b[6], b[7], b[8]);
-}
-
-bool win(char b[]) {
-    int w[8][3] = {
-        {0,1,2}, {3,4,5}, {6,7,8},
-        {0,3,6}, {1,4,7}, {2,5,8},
-        {0,4,8}, {2,4,6}
-    };
-
-    for (int i = 0; i < 8; i++) {
-        int a = w[i][0], c = w[i][1], d = w[i][2];
-        if (b[a] != ' ' && b[a] == b[c] && b[c] == b[d])
-            return true;
-    }
-
-    return false;
 }
